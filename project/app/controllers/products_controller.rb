85b8a6d075d5cbc165ff4b93a80abba225d455c7
Alexis <ianalexis709@hotmail.com>
2015-08-26 22:49:07 +0000

    ya se puede subir imagenes con productos, pero hay un problema con las categorias

--- project/app/controllers/products_controller.rb
+++ project/app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :trade_mark_id, :supplier_id)
+      params.require(:product).permit(:name, :description, :price, :category_id, :trade_mark_id, :supplier_id, :picture)
     end
 end
--- project/app/models/product.rb
+++ project/app/models/product.rb
@@ -2,4 +2,20 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :trade_mark
   belongs_to :supplier
+  
+  
+  #picture as attribute
+has_attached_file :picture,
+    :styles => { :medium =>"300x300>", :thumb=>"100x100>"},
+    :default_url => "/images/:style/missing.png"
+
+
+    validates :picture, attachment_presence: true, presence: true
+    validates_attachment :picture,
+    presence: true,
+    content_type: { content_type: ["image/jpeg","image/gif","image/png"]}
+   
+
 end
+
+
