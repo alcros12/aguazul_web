53288f1137464c0bf6bca173ed307c28d27b4491
Alexis <ianalexis709@hotmail.com>
2015-09-06 15:14:18 +0000

    ahora con rails_admin

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get '/index', to: 'home#index'
   get 'catalog', to: 'products#catalog'
