7e7b648086237d295c765b9c25328aeab35b326b
Alexis <ianalexis709@hotmail.com>
2015-09-06 04:00:21 +0000

    cambie los botones de login y logout

--- project/db/schema.rb
+++ project/db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150905210812) do
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",                 limit: 255
+    t.string   "name",                 limit: 50
     t.text     "Category_description", limit: 65535
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
