341a3f66faa0c831f9f73cf973c3fe864b1e2ba9
carlosmere <carlosmeremanchego@outlook.com>
2015-11-25 05:21:12 +0000

    final

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  #mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users 
   get '/index', to: 'home#index'
   get '/contactus', to: 'home#contact'
